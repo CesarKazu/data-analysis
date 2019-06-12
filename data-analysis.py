ec3a0a804824c744b606cb1ee74f917f3c932eaf
cesar.ando <cesar.ando@simptec.com.br>
2019-06-12 17:58:37 -0300

    commit comentarios

--- data-analysis.py
+++ data-analysis.py
@@ -2,6 +2,11 @@ import pandas as pd
 from pandas import Series, DataFrame
 import folium
 from folium import plugins
+import seaborn as sb
+low_memory=False
+%matplotlib inline
+pd.options.display.max_columns = 80
+pd.options.display.max_rows = 90
 
 path = 'datasets/'
 filenames = {'2018_1', '2018_2', '2018_3', '2018_4', '2018_5', '2018_6', '2018_7', '2018_8', '2018_9', '2018_10', '2018_11', '2018_12', '2019_1', '2019_2', '2019_3', '2019_4'}
@@ -19,8 +24,26 @@ def populaDataFrame():
 
 df = populaDataFrame()
 df.reset_index()
+df.shape
 
-geolocalizacoes = df[['LATITUDE', 'LONGITUDE']].copy()
+df.sample(10)
+
+df.groupby('PERIDOOCORRENCIA').size().sort_values().plot(kind='pie')
+
+df['PERIDOOCORRENCIA'].value_counts().to_frame()
+
+df.query('CIDADE == "S.JOSE DOS CAMPOS"')['BAIRRO'].value_counts().head(10).plot(kind='barh')
+
+df.query('CIDADE == "S.JOSE DOS CAMPOS"')['BAIRRO'].value_counts().to_frame().head(10)
+
+df.groupby('ANO_BO').size().sort_values().plot(kind='barh')
+
+df['ANO_BO'].value_counts().to_frame()
+
+df2019_4= pd.DataFrame(pd.read_csv((path+"2019_4"+extension), sep='\t', encoding='UTF-16 LE'))
+df2019_4.shape
+
+geolocalizacoes = df2019_4[['LATITUDE', 'LONGITUDE', 'PERIDOOCORRENCIA']].copy()
 geolocalizacoes.isnull().sum()
 
 geolocalizacoes.dropna(inplace=True)
@@ -31,13 +54,40 @@ lat = geolocalizacoes['LATITUDE'].values
 
 long = geolocalizacoes['LONGITUDE'].values
 
-mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=12)
+periodo = geolocalizacoes['PERIDOOCORRENCIA'].values
+
+colors = {
+ 'DE MADRUGADA': 'blue',
+ 'PELA MANHÃ': 'green',
+ 'A TARDE': 'orange',
+ 'A NOITE': 'red',
+ 'EM HORA INCERTA': 'purple',
+}
+
+mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=10)
+cont = 0
+for la,lo,p in zip(lat,long,periodo):
+    folium.Marker(
+        location=[float(la.replace(",", ".")), float(lo.replace(",", "."))],
+        icon=folium.Icon(color=colors[p])
+    ).add_to(mapa)
+
+mapa
+
+geolocalizacoes = df[['LATITUDE', 'LONGITUDE', 'PERIDOOCORRENCIA']].copy()
+geolocalizacoes.isnull().sum()
+
+geolocalizacoes.dropna(inplace=True)
+geolocalizacoes.isnull().sum()
+
+coordenadas = []
+lat = geolocalizacoes['LATITUDE'].values
+
+long = geolocalizacoes['LONGITUDE'].values
+
+mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=10)
  
 for la,lo in zip(lat,long):
-    folium.Marker(
-        location=[la, lo],
-    ).add_to(mapa)
     coordenadas.append([float(la.replace(",", ".")),float(lo.replace(",", "."))])
     
 mapa.add_child(plugins.HeatMap(coordenadas))
-mapa.add_child(coordenadas)
