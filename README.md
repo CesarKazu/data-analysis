f1183d09cc25b768aae1ffae9cbfe62b2eae2f76
cesar.ando <cesar.ando@simptec.com.br>
2019-06-14 14:18:28 -0300

    atualizacao readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# data-analysis
+# Análise de dados - Celulares Roubados
+
+<p>Análise de dados de celulares roubados registrados pelo departamento da DEINTER de São José dos Campos nos meses de Janeiro de 2018 à Abril de 2019.</p>
+<p>Com as informações cedidas pelo portal de transparencia do governo do estado de São Paulo é possivel evidenciar os seguintes itens:</p>
+<ul>
+	<li>Período do dia com maior frequência de roubos;</li>
+	<li>Bairros com maior frequência de roubos;</li>
+	<li>Quantidade de BO's em cada ano;</li>
+	<li>Mapa com marcadores indicando o local e o período de acontecimento dos roubos;</li>
+	<li>Mapa de calor mostrando as regiões com maiores indices de roubos;</li>
+</ul>
+
+# Fontes
+
+<ul>
+  <li>Portal da transparencia do Governo do estado de São Paulo: http://www.ssp.sp.gov.br/transparenciassp/</li>
+  <li>Documentação da biblioteca Pandas: https://pandas.pydata.org/pandas-docs/stable/</li>
+  <li>Documentação da biblioteca Folium: https://python-visualization.github.io/folium/</li>
+  <li>Documentação da biblioteca Selenium: https://selenium-python.readthedocs.io/</li>
+</ul>
+
+# Antes de executar
+
+<p>É necessário realizar a instalação das bibliotecas:</p>
+<strong>pip install requests beautifulsoup4 spotipy pdfminer3k selenium twitter wbdata pandas matplotlib lxml tweepy uber-rides xlrd PyPDF2 pytrends seaborn numpy ipython jupyter folium</strong>
--- data-analysis.py
+++ data-analysis.py
@@ -1,3 +1,11 @@
+#Analise de dados de celulares roubados registrados 
+#pelo departamento da DEINTER de São José dos Campos 
+#de Janeiro de 2018 à Abril de 2019.
+#Utilizando as informações cedidas pelo portal de transparencia do governo do estado de São Paulo 
+#é possivel evidenciar as regiões com maiores indices de roubo de celulares 
+#e assim aplicar uma solução de segurança. 
+#Projeto elaborado para a materia de Graduação de Projeto de Banco de Dados Não Estruturados na FATEC de São José dos Campos.
+
 import pandas as pd
 from pandas import Series, DataFrame
 import folium
@@ -12,6 +20,7 @@ path = 'datasets/'
 filenames = {'2018_1', '2018_2', '2018_3', '2018_4', '2018_5', '2018_6', '2018_7', '2018_8', '2018_9', '2018_10', '2018_11', '2018_12', '2019_1', '2019_2', '2019_3', '2019_4'}
 extension = '.xls'
 
+#Função para percorrer os arquivos de dados e popular o dataframe.
 def populaDataFrame():
     dfList = list()
 
@@ -25,29 +34,40 @@ def populaDataFrame():
 df = populaDataFrame()
 df.reset_index()
 df.shape
+#Quantidade de linhas e colunas no dataframe.
 
 df.sample(10)
+#10 linhhas aleatórias e seus dados.
 
 df.groupby('PERIDOOCORRENCIA').size().sort_values().plot(kind='pie')
+#Gráfico de pizza com os períodos de ocorrência dos roubos de celulares.
 
 df['PERIDOOCORRENCIA'].value_counts().to_frame()
+#Dados quantitativos com os períodos de ocorrência dos roubos de celulares.
 
 df.query('CIDADE == "S.JOSE DOS CAMPOS"')['BAIRRO'].value_counts().head(10).plot(kind='barh')
+#Gráfico de barras horizontais contendo os bairros de SJC com mais ocorrência dos roubos de celulares.
 
 df.query('CIDADE == "S.JOSE DOS CAMPOS"')['BAIRRO'].value_counts().to_frame().head(10)
+#Dados quantitativos contendo os bairros de SJC com mais ocorrência dos roubos de celulares.
 
 df.groupby('ANO_BO').size().sort_values().plot(kind='barh')
+#Gráfico de barras horizontais contendo a quantidade de BO's por ano de SJC sos roubos de celulares.
 
 df['ANO_BO'].value_counts().to_frame()
+#Dados quantitativos contendo a quantidade de BO's por ano de SJC sos roubos de celulares.
 
 df2019_4= pd.DataFrame(pd.read_csv((path+"2019_4"+extension), sep='\t', encoding='UTF-16 LE'))
 df2019_4.shape
+#Populando o dataframe com o arquivo de dados de 2019-4 e exibindo a quantidade de linhas e colunas.
 
 geolocalizacoes = df2019_4[['LATITUDE', 'LONGITUDE', 'PERIDOOCORRENCIA']].copy()
 geolocalizacoes.isnull().sum()
+#Copiando o dataframe com as informações que iremos utilizar e exibindo a quantidade de campos nulos.
 
 geolocalizacoes.dropna(inplace=True)
 geolocalizacoes.isnull().sum()
+#Removendo os campos nulos e exibindo novamente a quantidade de campos nulos.
 
 coordenadas = []
 lat = geolocalizacoes['LATITUDE'].values
@@ -64,7 +84,7 @@ colors = {
  'EM HORA INCERTA': 'purple',
 }
 
-mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=10)
+mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=12)
 cont = 0
 for la,lo,p in zip(lat,long,periodo):
     folium.Marker(
@@ -74,20 +94,68 @@ for la,lo,p in zip(lat,long,periodo):
 
 mapa
 
+#Através da biblioteca do Folium criamos um novo mapa, marcamos os roubos de celulares de abril 2019
+#com as latitudes e longitudes do dataframe e definimos a cor do marcador através do período de ocorrência.
+
+import os
+import time
+from selenium import webdriver
+
+delay=5
+fn='html/mapa1.html'
+tmpurl='file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=fn)
+mapa.save(fn)
+#Salvando o mapa em um arquivo html
+
+browser = webdriver.Firefox()
+browser.get(tmpurl)
+time.sleep(delay)
+browser.save_screenshot('img/mapa1.png')
+browser.quit()
+#Utilizando do selenium para abrir o arquivo .html no navegador e tirar um screenshot do mapa
+
+from IPython.display import Image
+Image(filename="img/mapa1.png")
+
 geolocalizacoes = df[['LATITUDE', 'LONGITUDE', 'PERIDOOCORRENCIA']].copy()
 geolocalizacoes.isnull().sum()
+#Copiando o dataframe com as informações que iremos utilizar e exibindo a quantidade de campos nulos.
 
 geolocalizacoes.dropna(inplace=True)
 geolocalizacoes.isnull().sum()
+#Removendo os campos nulos e exibindo novamente a quantidade de campos nulos.
 
 coordenadas = []
 lat = geolocalizacoes['LATITUDE'].values
 
 long = geolocalizacoes['LONGITUDE'].values
 
-mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=10)
+mapa = folium.Map(location=[-23.1791, -45.8872],tiles='OpenStreetMap',zoom_start=12)
  
 for la,lo in zip(lat,long):
     coordenadas.append([float(la.replace(",", ".")),float(lo.replace(",", "."))])
     
 mapa.add_child(plugins.HeatMap(coordenadas))
+#Através da biblioteca do Folium criamos um novo mapa e populamos ele com as latitudes e longitudes do dataframe
+#Utilizando um mapa de calor marcamos os pontos de roubos de celulares de janeiro de 2018 à  abril 2019
+
+import os
+import time
+from selenium import webdriver
+
+delay=5
+fn='html/mapa2.html'
+tmpurl='file://{path}/{mapfile}'.format(path=os.getcwd(),mapfile=fn)
+mapa.save(fn)
+#Salvando o mapa em um arquivo html
+
+browser = webdriver.Firefox()
+browser.get(tmpurl)
+time.sleep(delay)
+browser.save_screenshot('img/mapa2.png')
+browser.quit()
+#Utilizando do selenium para abrir o arquivo .html no navegador e tirar um screenshot do mapa
+
+from IPython.display import Image
+Image(filename="img/mapa2.png")
+#Carregando a imagem para exibição no github
